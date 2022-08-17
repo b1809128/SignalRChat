4d04ff88aea023d472f24067e63f09b525139c14
b1809128 <nghoquochuy0902@gmail.com>
2022-08-17 16:26:07 +0700

    update cors

--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
 using SignalRChat.Hubs;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.Services.AddSignalR();
+
+builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("ClientPermission", policy =>
+        {
+            policy.AllowAnyHeader()
+                .AllowAnyMethod()
+                .WithOrigins("http://localhost:3000")
+                .AllowCredentials();
+        });
+    });
 
 var app = builder.Build();
 
@@ -12,7 +22,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
-
+app.UseCors("ClientPermission");
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
--- readme.txt
+++ readme.txt
@@ -4,4 +4,4 @@ dotnet new webapp --no-https
 libman install @aspnet/signalr -p unpkg -d wwwroot/lib/signalr --files dist/browser/signalr.js --files dist/browser/signalr.min.js
 
 dotnet run => Startup.cs
-dotnet watch run --project SignalrSketchPad.csproj
+dotnet watch run --project SignalRChat.csproj
